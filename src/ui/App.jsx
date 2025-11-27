59b5d9d0e6c8a53735bbc27a0d220b21d7dda778
Matt Currier <fingerskier@gmail.com>
2025-11-27 11:06:05 -0600

    Remove in-page navigation bar

--- src/ui/App.jsx
+++ src/ui/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import MenuBar from './MenuBar';
 import Home from './Home';
 import Settings from './Settings';
 
@@ -8,7 +7,6 @@ export default function App() {
   return (
     <Router>
       <div style={{ minHeight: '100vh', backgroundColor: '#f8f9fa' }}>
-        <MenuBar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/settings" element={<Settings />} />
