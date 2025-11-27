cb46ee524b987f7df53d0362f6e175db5f295080
Matt Currier <fingerskier@gmail.com>
2025-11-27 11:01:30 -0600

    Externalize pg for electron main build (#1)

--- vite.main.config.mjs
+++ vite.main.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      // Prevent bundling of native dependencies that can break when
+      // initialized before Electron loads them in the main process.
+      external: ['pg', 'pg-native'],
+    },
+  },
+});
